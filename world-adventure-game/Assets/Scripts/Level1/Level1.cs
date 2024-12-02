7fcff89e943ac81203e0976b7ed1670436f6bba8
Yeun Haur Kang <yh20030816@gmail.com>
2024-12-02 04:13:57 -0300

    fix: removida linhas de código de depuração

--- world-adventure-game/Assets/Scripts/Level1/Level1.cs
+++ world-adventure-game/Assets/Scripts/Level1/Level1.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 public class Level1 : MonoBehaviour
 {
     private PlayerBehavior player;
-    private CollectiblesManager cm;
-    private int sp;
-    private int cs;
-    private int ts;
 
     private void Awake()
     {
         player = FindFirstObjectByType<PlayerBehavior>();
-        cm = FindAnyObjectByType<CollectiblesManager>();
-        sp = cm.GetSelectedPlayer();
-        cs = PlayerPrefs.GetInt("CurrentScore" + sp, 0);
-        ts = PlayerPrefs.GetInt("Score" + sp, 0);
     }
 
-    private void Update()
-    {
-        cs = PlayerPrefs.GetInt("CurrentScore" + sp, 0);
-        ts = PlayerPrefs.GetInt("Score" + sp, 0);
-        print($"Selected Player: {sp} | Current Score: {cs} | Total Score {ts}.");
-    }
     private void Start()
     {
         AudioManager.instance.level1Theme();
